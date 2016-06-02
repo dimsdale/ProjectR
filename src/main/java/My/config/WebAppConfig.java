49dc58aafd6393eec80c5ca7e4c20b753f03bd8f
Dimsdale <dimmsdale93@gmail.com>
2016-06-02 13:03:26 +0300

    Bugs fixed

--- src/main/java/My/config/WebAppConfig.java
+++ src/main/java/My/config/WebAppConfig.java
@@ -9,9 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -19,8 +17,6 @@ import org.springframework.transaction.annotation.TransactionManagementConfigure
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import javax.annotation.Resource;
-import javax.sql.DataSource;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.contacts (id, name) VALUES ('12', 'Dmitriy');
+INSERT INTO public.contacts (id, name) VALUES ('13', 'Aleksey');
+INSERT INTO public.contacts (id, name) VALUES ('14', 'Zina');
+INSERT INTO public.contacts (id, name) VALUES ('15', 'Stepan');
+INSERT INTO public.contacts (id, name) VALUES ('16', 'Artem');
+INSERT INTO public.contacts (id, name) VALUES ('17', 'Nataliya');
+INSERT INTO public.contacts (id, name) VALUES ('18', 'Mykhaylo');
+INSERT INTO public.contacts (id, name) VALUES ('19', 'Maxim');
+INSERT INTO public.contacts (id, name) VALUES ('20', 'Ivan');
+INSERT INTO public.contacts (id, name) VALUES ('21', 'Alyona');
+
+
