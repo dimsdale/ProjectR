334326cabc6046638168cefd631a95644395c7a6
Dimsdale <dimmsdale93@gmail.com>
2016-06-02 12:21:35 +0300

    Bugs fixed

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE contactschema;
+
+CREATE DATABASE contactschema
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Russian_Russia.1251'
+       LC_CTYPE = 'Russian_Russia.1251'
+
